b8312866fecf18bb24f672afae0c913d5b649d2d
Ivango <ognavi@gmail.com>
2020-06-12 14:46:15 -0300

    setup do projeto

---
+++ src/main/java/com/ivango/testeserasa/TesteserasaApplication.java
@@ -0,0 +1,13 @@
+package com.ivango.testeserasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteserasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteserasaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivango/testeserasa/TesteserasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivango.testeserasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteserasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
